0ad50322616a0ce93c5646b3dc9beed8a352dc86
Harry Ganz <harryyehudah.ganz@gartner.com>
2017-09-14 15:25:26 -0400

    Removes previous main method

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 """Main module that runs the game"""
-from game import Game, RED, BLUE, GREEN
-from _mocks import MockLight, MockButton
 
 if __name__ == '__main__':
-    GAME = Game(lights={RED: (MockLight(1), MockButton(2))})
-    GAME.add_light_with_button(GREEN, MockLight(3), MockButton(4))
-    GAME.start()
+    pass
