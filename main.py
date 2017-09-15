3960374112092be0e7125e43193db10c1f5e24f5
Harry Ganz <hganz@harryganz.com>
2017-09-14 22:24:27 -0400

    Starting Over

--- main.py
+++
@@ -1,9 +0,0 @@
-"""Main module that runs the game"""
-from gpiozero import LED, Buzzer, Button
-from game import Game
-
-if __name__ == '__main__':
-    GAME = Game(lights=[(LED(17), Button(27))], buzzer=Buzzer(4))
-    GAME.start_game()
-    while True:
-        continue
