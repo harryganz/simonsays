32620df212fa983713c54aca14393bd8eeb19a89
Harry Ganz <hganz@harryganz.com>
2017-09-16 16:56:16 -0400

    Add leds to game

--- game.py
+++ game.py
@@ -1,17 +1,22 @@
 """
 Simon says game
 """
+import gpiozero
 
 class Game(object):
     """
     Instance of simon says game
     """
 
-    def __init__(self):
+    def __init__(self, light_pins, led=gpiozero.LED):
         """
         Create simon says game instance
+        @param light_pins: array of pins to use for lights
+        @param led: Class to use to handle lights (default gpiozero.LED)
         """
-        pass
+        self.lights = []
+        for pin in light_pins:
+            self.lights.append(led(pin))
 
     def start(self):
         """
@@ -21,12 +26,13 @@ class Game(object):
         try:
             print("Starting simonsays game")
             while True:
-                continue
+                for light in self.lights:
+                    light.blink(0.3, 0.3, 1, background=False)
         except KeyboardInterrupt:
             print("Stopping simonsays game")
 
 
 # Start game if run as executable
 if __name__ == '__main__':
-    GAME = Game()
+    GAME = Game([17, 5, 19])
     GAME.start()
